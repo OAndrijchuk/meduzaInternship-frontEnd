d8fcc0528d49619de61853f3600b9d28adc2a5c6
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-10-24 17:48:31 +0300

    initial-commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+import styles from './page.module.css'
+
+export default async function Home() {
+  return (
+    <main className={styles.main}>
+    
+      <div className={styles.center}>
+        <h1>Welcome to meduzzen-internship-front app</h1>
+      </div>
+     
+    </main>
+  )
+}
