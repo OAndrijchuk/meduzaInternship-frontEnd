e3c4a79cdb80a75fe72a532163f75977adef7119
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-11-15 01:49:11 +0200

    fix modal style

--- Components/Modal/TheModal.styled.tsx
+++ Components/Modal/TheModal.styled.tsx
@@ -3,7 +3,7 @@ import { styled } from "@mui/material/styles";
 import Box from '@mui/material/Box';
 
 export const ModalCont = styled(Box)({
-  position: 'absolute' as 'absolute',
+  position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
