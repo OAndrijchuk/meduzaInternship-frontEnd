998b556f5bf50169318beca23034cd88c84344c0
Oleh Andrijchuk <107946653+OAndrijchuk@users.noreply.github.com>
2023-11-15 01:59:45 +0200

    fixedModalStyled2

--- Components/Modal/TheModal.styled.tsx
+++ Components/Modal/TheModal.styled.tsx
@@ -2,20 +2,20 @@
 import { styled } from "@mui/material/styles";
 import Box from '@mui/material/Box';
 
-export const ModalCont = styled(Box)({
+export const ModalCont = styled(Box)(({ theme }) => ({
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   maxWidth: '90vw',
   backgroundColor: 'white',
-  padding:15,
+  padding: 15,
   border: '2px solid #000',
-  borderRadius:25,
-  boxShadow: 24,
-  p: 4,
-});
-export const ModalOverlay = styled(Box)({
+  borderRadius: theme.spacing(3), 
+  boxShadow: theme.shadows[24],
+  p: theme.spacing(4), 
+}));
+export const ModalOverlay = styled(Box)(({ theme }) => ({
   position: 'fixed',
   top: 0,
   left: 0,
@@ -26,4 +26,4 @@ export const ModalOverlay = styled(Box)({
   alignItems: 'center',
   backgroundColor: 'rgba(34, 13, 91, 0.23)',
   backdropFilter: 'blur(3.5px)',
-});
+}));
